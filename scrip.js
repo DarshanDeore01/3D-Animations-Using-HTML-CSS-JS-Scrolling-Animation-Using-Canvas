18015e39a84691cd3f36b0339a74f0ed63068a33
Your Name <your.email@example.com>
2025-01-10 16:04:05 +0530

    Started with Html JS Css And Added 3D Canvas Model to the Project

---
+++ scrip.js
@@ -0,0 +1,32 @@
+gsap.to("#page1", {
+    scrollTrigger: {
+        trigger: `#page1`,
+        start: `tot top`,
+        end: `bottom top`,
+        markers: true,
+        pin: true,
+        scroller:`#main`
+    },
+})
+
+gsap.to("#page2", {
+    scrollTrigger: {
+        trigger: `#page2`,
+        start: `tot top`,
+        end: `bottom top`,
+        markers: true,
+        pin: true,
+        scroller:`#main`
+    },
+})
+
+gsap.to("#page3", {
+    scrollTrigger: {
+        trigger: `#page3`,
+        start: `tot top`,
+        end: `bottom top`,
+        markers: true,
+        pin: true,
+        scroller:`#main`
+    },
+})
